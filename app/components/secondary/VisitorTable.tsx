55c87e0a6aa0dfe50ee391623546b90832b7f6b1
Sebastian Martin <smartin@lumafintech.com>
2024-03-27 12:34:16 -0400

    fixing css

--- app/components/secondary/VisitorTable.tsx
+++ app/components/secondary/VisitorTable.tsx
@@ -18,28 +18,25 @@ export const VisitorTable = ({
         </tr>
       </thead>
       <tbody>
-        {listOfAllVisitors
-          ?.sort((a, b) => (a.age < b.age ? -1 : 1))
-          .reverse()
-          .map((visitor, idx) => (
-            <tr
-              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-              key={idx}
+        {listOfAllVisitors?.map((visitor, idx) => (
+          <tr
+            className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+            key={idx}
+          >
+            <th
+              scope="row"
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
             >
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                {visitor.name}
-              </th>
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                {visitor.age}
-              </th>
-            </tr>
-          ))}
+              {visitor.name}
+            </th>
+            <th
+              scope="row"
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              {visitor.age}
+            </th>
+          </tr>
+        ))}
       </tbody>
     </table>
   </div>
